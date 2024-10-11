4e128a9cd92f747c85329dbc29119024e0efbc28
Kitupala <kimmo.tuupanen@gmail.com>
2024-10-11 10:12:33 +0300

    Add Header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
 
 import "./globals.css";
+import Header from "@/app/components/Header";
 
 const alpino = localFont({
   src: "../../public/fonts/Alpino-Variable.woff2",
@@ -19,7 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={alpino.variable}>
-      <body>{children}</body>
+      <body className="overflow-x-hidden bg-yellow-300">
+        <Header />
+        <main>{children}</main>
+      </body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
   );
