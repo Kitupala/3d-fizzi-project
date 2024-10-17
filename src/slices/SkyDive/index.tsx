f8fde63c25959c41b0f45cc16ed86992af76ee22
Kitupala <kimmo.tuupanen@gmail.com>
2024-10-17 16:19:06 +0300

    Add new Scene and animate skydive

--- src/slices/SkyDive/index.tsx
+++ src/slices/SkyDive/index.tsx
@@ -1,22 +1,28 @@
+"use client";
+
+import { Bounded } from "@/app/components/Bounded";
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
+import Scene from "./Scene";
+import { View } from "@react-three/drei";
 
-/**
- * Props for `SkyDive`.
- */
 export type SkyDiveProps = SliceComponentProps<Content.SkyDiveSlice>;
 
-/**
- * Component for "SkyDive" Slices.
- */
 const SkyDive = ({ slice }: SkyDiveProps): JSX.Element => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="skydive h-screen"
     >
-      Placeholder component for sky_dive (variation: {slice.variation}) Slices
-    </section>
+      <h2 className="sr-only">{slice.primary.sentence}</h2>
+      <View className="h-screen w-screen">
+        <Scene
+          flavor={slice.primary.flavor}
+          sentence={slice.primary.sentence}
+        />
+      </View>
+    </Bounded>
   );
 };
 
